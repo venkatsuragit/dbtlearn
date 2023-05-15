f6b73ff1e1f5d513609c2d786ed960edfd3aedfc
vnktsura@gmail.com <vnktsura@gmail.com>
2023-05-15 00:46:35 +0000

    vom

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,13 +7,13 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
-
+--{{ config(materialized='table') }}
+{{config(materialized='incremental',unique_key='id')}}
 with source_data as (
 
     select 1 as id
     union all
-    select null as id
+    select 200 as id
 
 )
 
